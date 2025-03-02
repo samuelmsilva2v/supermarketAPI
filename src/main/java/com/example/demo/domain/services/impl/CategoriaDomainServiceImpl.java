606ce52459b28874b89a8f39b92dee8bee5691b8
samuelmsilva2v <samuelmsilva@outlook.com.br>
2025-03-02 16:33:56 -0300

    adjusted controllers, model mapper, implemented service methods

--- src/main/java/com/example/demo/domain/services/impl/CategoriaDomainServiceImpl.java
+++ src/main/java/com/example/demo/domain/services/impl/CategoriaDomainServiceImpl.java
@@ -3,43 +3,67 @@ package com.example.demo.domain.services.impl;
 import java.util.List;
 import java.util.UUID;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.application.dtos.CategoriaRequestDto;
 import com.example.demo.application.dtos.CategoriaResponseDto;
+import com.example.demo.domain.models.entities.Categoria;
 import com.example.demo.domain.services.interfaces.CategoriaDomainService;
+import com.example.demo.infrastructure.repositories.CategoriaRepository;
 
 @Service
 public class CategoriaDomainServiceImpl implements CategoriaDomainService {
 
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	@Autowired
+	private ModelMapper modelMapper;
+
 	@Override
 	public String registrarCategoria(CategoriaRequestDto request) {
-		// TODO Auto-generated method stub
-		return null;
+
+		var categoria = new Categoria();
+		categoria.setId(UUID.randomUUID());
+		categoria.setNome(request.getNome());
+
+		categoriaRepository.save(categoria);
+
+		return "Categoria registrada com sucesso!";
 	}
 
 	@Override
 	public CategoriaResponseDto editarCategoria(UUID id, CategoriaRequestDto request) {
-		// TODO Auto-generated method stub
-		return null;
+
+		var categoria = categoriaRepository.findById(id).get();
+		categoria.setNome(request.getNome());
+
+		categoriaRepository.save(categoria);
+
+		return modelMapper.map(categoria, CategoriaResponseDto.class);
 	}
 
 	@Override
 	public String excluirCategoria(UUID id) {
-		// TODO Auto-generated method stub
-		return null;
+
+		categoriaRepository.deleteById(id);
+
+		return "Categoria excluída com sucesso!";
 	}
 
 	@Override
 	public CategoriaResponseDto consultarCategoriaPorId(UUID id) {
-		// TODO Auto-generated method stub
-		return null;
+
+		return modelMapper.map(categoriaRepository.findById(id).get(), CategoriaResponseDto.class);
 	}
 
 	@Override
 	public List<CategoriaResponseDto> consultarCategorias() {
-		// TODO Auto-generated method stub
-		return null;
+
+		return categoriaRepository.findAll().stream()
+				.map(categoria -> modelMapper.map(categoria, CategoriaResponseDto.class)).toList();
 	}
 
 }
--- src/main/java/com/example/demo/domain/services/impl/ProdutoDomainServiceImpl.java
+++ src/main/java/com/example/demo/domain/services/impl/ProdutoDomainServiceImpl.java
@@ -3,43 +3,73 @@ package com.example.demo.domain.services.impl;
 import java.util.List;
 import java.util.UUID;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.application.dtos.ProdutoRequestDto;
 import com.example.demo.application.dtos.ProdutoResponseDto;
+import com.example.demo.domain.models.entities.Produto;
 import com.example.demo.domain.services.interfaces.ProdutoDomainService;
+import com.example.demo.infrastructure.repositories.CategoriaRepository;
+import com.example.demo.infrastructure.repositories.ProdutoRepository;
 
 @Service
 public class ProdutoDomainServiceImpl implements ProdutoDomainService {
 
+	@Autowired
+	private ProdutoRepository produtoRepository;
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+
+	@Autowired
+	private ModelMapper modelMapper;
+
 	@Override
 	public String registrarProduto(ProdutoRequestDto request) {
-		// TODO Auto-generated method stub
-		return null;
+
+		var produto = modelMapper.map(request, Produto.class);
+		produto.setId(UUID.randomUUID());
+		produto.setCategoria(categoriaRepository.findById(request.getCategoriaId()).get());
+
+		produtoRepository.save(produto);
+
+		return "Produto cadastrado com sucesso!";
 	}
 
 	@Override
 	public ProdutoResponseDto atualizarProduto(UUID id, ProdutoRequestDto request) {
-		// TODO Auto-generated method stub
-		return null;
+
+		var produto = produtoRepository.findById(id).get();
+
+		produto.setNome(request.getNome());
+		produto.setPreco(request.getPreco());
+		produto.setQuantidade(request.getQuantidade());
+		produto.setCategoria(categoriaRepository.findById(request.getCategoriaId()).get());
+
+		produtoRepository.save(produto);
+
+		return modelMapper.map(produto, ProdutoResponseDto.class);
 	}
 
 	@Override
 	public String excluirProduto(UUID id) {
-		// TODO Auto-generated method stub
-		return null;
+
+		produtoRepository.deleteById(id);
+
+		return "Produto excluído com sucesso!";
 	}
 
 	@Override
 	public ProdutoResponseDto consultarProdutoPorId(UUID id) {
-		// TODO Auto-generated method stub
-		return null;
+		return modelMapper.map(produtoRepository.findById(id), ProdutoResponseDto.class);
 	}
 
 	@Override
 	public List<ProdutoResponseDto> consultarProdutos() {
-		// TODO Auto-generated method stub
-		return null;
+		return produtoRepository.findAll().stream().map(produto -> modelMapper.map(produto, ProdutoResponseDto.class))
+				.toList();
 	}
 
 }
