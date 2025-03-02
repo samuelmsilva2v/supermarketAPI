0997c2d2523b9482517658c20c1c952dee38c124
samuelmsilva2v <samuelmsilva@outlook.com.br>
2025-03-02 15:02:55 -0300

    db docker compose

---
+++ src/main/java/com/example/demo/domain/models/entities/Categoria.java
@@ -0,0 +1,12 @@
+package com.example.demo.domain.models.entities;
+
+import java.util.UUID;
+
+import lombok.Data;
+
+@Data
+public class Categoria {
+
+	private UUID id;
+	private String nome;
+}
---
+++ src/main/java/com/example/demo/infrastructure/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.infrastructure.repositories;
+
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.demo.domain.models.entities.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, UUID> {
+	
+}
